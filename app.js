ca669210cd502b3d426c8e8d9fdbe416633961c9
Vaerilius <tjohannest@gmail.com>
2019-02-27 08:32:22 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const github = new Github;
+const ui = new UI;
+const searcUser = document.querySelector('#searchUser');
+
+searcUser.addEventListener('keyup', (e) => {
+  const userText = e.target.value;
+
+  if (userText !== '') {
+
+    github.getUser(userText)
+    .then(data => {
+      console.log(data);
+      if (data.profile.message === 'Not Found') {
+        //show alert
+        ui.showAlert('User not found', 'alert alert-danger');
+      } else {
+        //show profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+
+    })
+  }else {
+    //clear profile
+    ui.clearProfile();
+  }
+});
